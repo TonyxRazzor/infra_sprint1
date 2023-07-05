d244faf4a2e63c3affba32a89581981fe0bfb13d
TonyxRazzor <120474556+TonyxRazzor@users.noreply.github.com>
2023-07-05 17:58:37 +0300

    Update settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('TOKEN', 'SECRET')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['84.252.142.244', '127.0.0.1', 'localhost', 'kurva-maska.hopto.org']
 
